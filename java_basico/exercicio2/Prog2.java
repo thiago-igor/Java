3e1138963a62eeafbb06e56d41d6f4025dc5265b
thiago-igor <thiago.maia0606@gmail.com>
2021-10-13 16:52:01 -0300

    atualizaçao

---
+++ java_basico/exercicio2/Prog2.java
@@ -0,0 +1,13 @@
+package java_basico.exercicio2;
+
+import java.util.Date;
+
+public class Prog2 {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema e:");
+        System.out.println(relogio.toString());
+    }
+    
+}
+
