315da535a8fb0d87697ab8d9e0b0a183f29a45b6
thiago-igor <thiago.maia0606@gmail.com>
2021-10-25 15:42:42 -0300

    atualizar

--- java_poo/aula3/visibilidade_e_encapsulamento/encapsulamento/ClasseEncapsulada.java
+++ java_poo/aula3/visibilidade_e_encapsulamento/encapsulamento/ClasseEncapsulada.java
@@ -3,7 +3,7 @@ package java_poo.aula3.visibilidade_e_encapsulamento.encapsulamento;
 public class ClasseEncapsulada {
 	
 	// Definiçao de atributos privados
-	private int a, b;
+	private int a;
 	
 	// Definiçao de metodos acessores publicos
 	public int getA() { // serve para conseguir ler
