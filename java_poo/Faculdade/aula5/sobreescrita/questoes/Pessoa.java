5fe7f2bb1e8ae5bed31eb38d1bd2103f578ba38c
thiago-igor <thiago.maia0606@gmail.com>
2021-11-05 14:39:39 -0300

    atualizar

---
+++ java_poo/Faculdade/aula5/sobreescrita/questoes/Pessoa.java
@@ -0,0 +1,58 @@
+package java_poo.Faculdade.aula5.sobreescrita.questoes;
+
+public class Pessoa {
+
+    private String nome;
+	private String endereço;
+	private int idade;
+	private String cpf;
+	private String sexo;
+	private boolean andar;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEndereço() {
+        return endereço;
+    }
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public boolean isAndar() {
+        return andar;
+    }
+    public void setAndar(boolean andar) {
+        this.andar = andar;
+    }
+
+    
+}
