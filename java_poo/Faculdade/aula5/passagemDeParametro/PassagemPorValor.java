ef9233034eb4974557cd05ca7b54e25932adc4ea
thiago-igor <thiago.maia0606@gmail.com>
2021-11-07 12:34:37 -0300

    atualizaçao aula 5 faculdade

---
+++ java_poo/Faculdade/aula5/passagemDeParametro/PassagemPorValor.java
@@ -0,0 +1,18 @@
+package java_poo.Faculdade.aula5.passagemDeParametro;
+
+public class PassagemPorValor {
+	
+	public static void main(String[] args) {
+		int numero = 5; // definiçao de valor fora do metodo
+		int resultado = quadrado(numero);
+		System.out.println("N�mero: "+numero); // Imprime: 5
+		System.out.println("Resultado: "+resultado); // Imprime: 4
+	}
+	
+	public static int quadrado(int numero) {
+		numero = 2; // alteraçao de valor dentro do metodo
+		return numero*numero;
+	}
+
+	// podemos observar que ele altera a variavel do metodo, mas nao a global, pois ah uma copia da global para o metodo, se tornando assim, duas variaveis diferentes.
+}
