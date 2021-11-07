6675727742eb1eae1bdf9c97d2e374020f376fd3
thiago-igor <thiago.maia0606@gmail.com>
2021-11-07 13:28:41 -0300

    atualizaçao

---
+++ java_poo/Faculdade/aula5/passagemDeParametro/questoes/Qst4.java
@@ -0,0 +1,17 @@
+package java_poo.Faculdade.aula5.passagemDeParametro.questoes;
+
+public class Qst4 {
+
+    public static void main(String[] args){
+        imprimir(1,2,3); // PASSAGEM COM 3 PARAMETROS 
+        imprimir(1,2); // PASSAGEM COM DOIS PARAMETROS 
+        imprimir(); // PASSAGEM SEM PARAMETROS
+    }
+
+    public static void imprimir(int...inteiros){ // metodo com passagem de parametros variavel.
+        for(int num: inteiros){
+            System.out.println(num);
+        }
+    }
+    
+}
