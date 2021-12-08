5b8cca43b02aab5241c3c7ab3175651f83ce99c0
thiago-igor <thiago.maia0606@gmail.com>
2021-12-08 10:45:17 -0300

    atualizaçao

--- java_poo/Faculdade/aula7_Polimofismo/Equals/Valor.java
+++ java_poo/Faculdade/aula7_Polimofismo/Equals/Valor.java
@@ -1,18 +1,5 @@
 package java_poo.Faculdade.aula7_Polimofismo.Equals;
 
-/*public class Valor {
-	int v;
-	public Valor(int v) { this.v = v; }
-	public String toString() {
-		return ""+v;
-	}
-	public static void main(String[] args) {
-		Integer i = new Integer(100);
-		System.out.println(i); // 100
-		Valor v = new Valor(100);
-		System.out.println(v); // 100
-	}
-}*/
 public class Valor {
 	int v;
 	public Valor(int v) { this.v = v; }
