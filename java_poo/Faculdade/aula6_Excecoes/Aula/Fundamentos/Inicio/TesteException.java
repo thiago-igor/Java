22cac94ba6717b6eee6d40f2b2b3da46d097d970
thiago-igor <thiago.maia0606@gmail.com>
2022-01-18 10:39:37 -0300

    Atualizaçao

---
+++ java_poo/Faculdade/aula6_Excecoes/Aula/Fundamentos/Inicio/TesteException.java
@@ -0,0 +1,15 @@
+package java_poo.Faculdade.aula6_Excecoes.Aula.Fundamentos.Inicio;
+
+public class TesteException {
+	
+	public static void main (String[] args){
+		try{
+			// lançamento de excecao!
+			throw new Exception("Excecao teste");
+		}catch(Exception e){
+			// codigo de tratamento da exceçao
+			System.out.println("Capturada" +e);
+			e.printStackTrace();
+		}
+	}
+}
---
+++ java_poo/Faculdade/aula6_Excecoes/Aula/Fundamentos/Inicio/TesteLeituraNumero.java
@@ -0,0 +1,21 @@
+package java_poo.Faculdade.aula6_Excecoes.Aula.Fundamentos.Inicio;
+
+import java.util.Scanner;
+
+public class TesteLeituraNumero {
+	public static void main (String []args){
+		Scanner sc = new Scanner(System.in);
+		boolean ok = false;
+		while(ok==false){
+			System.out.println("Informe um numero:");
+			try{ // ponto do codigo que pode ocorrer um erro( ex: digitar uma palavra em vez do numero )
+				int numero = Integer.parseInt(sc.nextLine());
+				System.out.println("Numero informado:" + numero);
+				ok = true;
+			}catch(Exception e){// caso ocorra uma exeçao, sera chamado o "catch"
+				System.out.println("Voce nao forneceu um numero!");
+			}
+		}
+		sc.close();
+	}
+}
