664e56e2d08824fd0b4033930a710295f28795fb
thiago-igor <thiago.maia0606@gmail.com>
2022-01-18 11:09:47 -0300

    atualizaçao

--- java_poo/Faculdade/aula6_Excecoes/Aula/Fundamentos/exemplo1/Lampada.java
+++ java_poo/Faculdade/aula6_Excecoes/Aula/Fundamentos/exemplo1/Lampada.java
@@ -6,14 +6,16 @@ public class Lampada {
 	// 'f' -> fluorescente
 	// 'l' -> led
 	private char tipo;
-	public Lampada(char tipo) throws Exception {
-		if (tipo=='i' || tipo=='f' || tipo=='l')
+	public Lampada(char tipo) throws Exception{
+		if(tipo=='i' || tipo=='f' || tipo=='l'){
 			this.tipo = tipo;
+		}
 		else
-			throw new Exception(
-					"Tipo invalido. Use 'i', 'f' ou 'l'.");
+			throw new Exception("Tipo invalido. Use 'i', 'f' ou 'l' ");
 	}
-	public char getTipo() { return tipo; }
+	public char getTipo(){
+		return tipo;
+	}
 }
 
 /*public class Lampada {
