dcdfd974297ae9d779055431c36dd29367ac0d96
thiago-igor <thiago.maia0606@gmail.com>
2022-01-24 10:57:36 -0300

    atualização

--- java_poo/Faculdade/aula7_interfaces/interfacie/exemplo1/Carrao.java
+++ java_poo/Faculdade/aula7_interfaces/interfacie/exemplo1/Carrao.java
@@ -1,7 +1,7 @@
 package java_poo.Faculdade.aula7_interfaces.interfacie.exemplo1;
 
-public class Carrao implements VeiculoComPortas, // nos podemos ver que estamos implementando 3 classes
-	VeiculoComPortaMalas, VeiculoComTetoSolar {
+public class Carrao implements VeiculoComPortas, 
+	VeiculoComPortaMalas, VeiculoComTetoSolar { // nos podemos ver que estamos implementando 3 classes
 	public void abrirTetoSolar() {
 		// Alguma implementacao
 	}
