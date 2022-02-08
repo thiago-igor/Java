94b7f0a686e114a1ee7372f88dfa6ff0fb0c0036
thiago-igor <thiago.maia0606@gmail.com>
2022-02-08 17:30:22 -0300

    Atualizaçao

--- java_poo/Faculdade/Aula10_interface_Grafica/jOptionpane/TesteJOptionPane.java
+++ java_poo/Faculdade/Aula10_interface_Grafica/jOptionpane/TesteJOptionPane.java
@@ -10,30 +10,24 @@ public class TesteJOptionPane {
 		
 		while (continua) {
 			
-			String xStr = JOptionPane.showInputDialog(null,
-					"Informe um valor inteiro:", "Calculadora MDC", JOptionPane.QUESTION_MESSAGE);
-			
-			String yStr = JOptionPane.showInputDialog(null,
-					"Informe outro valor inteiro:", "Calculadora MDC", JOptionPane.QUESTION_MESSAGE);
-			
-			int x = Integer.parseInt(xStr);
-			int y = Integer.parseInt(yStr);
-			
-			Calculadora calc = new Calculadora();
-			int resultado = calc.mdc(x, y);
-			
-			JOptionPane.showMessageDialog(null,
-					"O MDC de "+xStr+" e "+yStr+" eh igual a "+resultado,
-					"Calculadora MDC", JOptionPane.INFORMATION_MESSAGE);
-			
-			int opcao = JOptionPane.showConfirmDialog(null,
-					"Voce deseja calcular outro MDC?",
-					"Calculadora MDC", JOptionPane.YES_NO_OPTION);
-			
-			if (opcao!=JOptionPane.YES_OPTION) {
+		String xStr = JOptionPane.showInputDialog(null, "Informe um valor inteiro", "Calculadora MDC", JOptionPane.QUESTION_MESSAGE); // aqui estamos fazendo uma interfaciel com parametro = null, mensagem = informe um valor inteiro , titulo = Calculadora mdc e o tipo da interface = QUESTION_MESSAGE
+
+		String yStr = JOptionPane.showInputDialog(null, "Informe outro valor inteiro", "Calculadora MDC", JOptionPane.QUESTION_MESSAGE);
+		
+
+		int x = Integer.parseInt(xStr); // transformando uma string em um inteiro, pois JOpitionpane retorna uma string
+		int y = Integer.parseInt(yStr);
+
+		Calculadora calc = new Calculadora();
+		int resultado = calc.mdc(x, y);
+
+			JOptionPane.showMessageDialog(null, "MDC de "+ xStr + " e " +yStr +" = "+ resultado, " Resposta(MDC)", JOptionPane.INFORMATION_MESSAGE);
+
+			int opcao = JOptionPane.showConfirmDialog(null, "Deseja calcular outro MDC?", "Calculadora MDC", JOptionPane.YES_NO_OPTION); //JOptionPane.YES_NO_OPTION = opçao de sim ou nao
+
+			if(opcao != JOptionPane.YES_OPTION ){
 				continua = false;
 			}
-			
 		}
 		
 	}
