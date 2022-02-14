e47355b6ce4ce37f38b0c0c697734db408dab3c4
thiago-igor <thiago.maia0606@gmail.com>
2022-02-14 16:38:22 -0300

    atualizaçao

---
+++ java_poo/Faculdade/Aula13_colecoes/HashSet/Exemplo.java
@@ -0,0 +1,19 @@
+package java_poo.Faculdade.Aula13_colecoes.HashSet;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.HashSet;
+import java.util.Set;
+public class Exemplo {
+
+    //com o HashSet nao podemos criar elementos repetidos no array
+	public static void main(String[] args) throws ParseException {
+		Set<Pessoa> conjuntoPessoas = new HashSet<Pessoa>();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Pessoa p1 = new Pessoa("Maria",sdf.parse("10/06/2006"));
+		Pessoa p2 = new Pessoa("Maria",sdf.parse("15/09/2000"));
+		conjuntoPessoas.add(p1); conjuntoPessoas.add(p2);
+		System.out.println(conjuntoPessoas);
+	}
+}
