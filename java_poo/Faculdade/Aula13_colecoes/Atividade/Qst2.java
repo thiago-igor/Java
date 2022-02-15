0f315a3851e7806a4571b87b1f633d0e8ffb1b55
thiago-igor <thiago.maia0606@gmail.com>
2022-02-15 11:50:52 -0300

    Atualizando qst2

--- java_poo/Faculdade/Aula13_colecoes/Atividade/Qst2.java
+++ java_poo/Faculdade/Aula13_colecoes/Atividade/Qst2.java
@@ -3,23 +3,30 @@ package java_poo.Faculdade.Aula13_colecoes.Atividade;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Qst2 {
     
     public static void main(String[] args) {
         List<Integer> listaNumeros = new LinkedList<Integer>();//com esse Array consigo adicionar ou excluir quantos elementos eu quiser 
+        String resp;
+        Scanner sc = new Scanner(System.in);
 
         //adicionando:
-        listaNumeros.add(10);
-        listaNumeros.add(5);
-        listaNumeros.add(6);
-        listaNumeros.add(7);
-        listaNumeros.add(8);
-        listaNumeros.add(9);
-        listaNumeros.add(20);
-        listaNumeros.add(34);
-        listaNumeros.add(1);
-        listaNumeros.add(5);
+        do{
+            System.out.println("digiteum numero:");
+            resp = sc.nextLine();
+            if(resp.equals("fim"))
+                break;
+            
+            try {
+               int numero = Integer.parseInt(resp);
+                listaNumeros.add(numero);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Digto nao permitido");
+            }
+        }while(true);
 
         //ordenando:
         Collections.sort(listaNumeros);// Sort: ordena o array
